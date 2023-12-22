2b496eed103280ff2e5d43a517e086ea889e35be
babduck0 <babduck0@naver.com>
2023-12-23 01:30:19 +0900

    TST8
    Signed-off-by: babduck0 <babduck0@naver.com>

--- index.js
+++ index.js
@@ -1,30 +1,30 @@
 var mySong=document.getElementById('mySong');
         var icon=document.getElementById('icon');
         var logo=document.getElementById('logo');
-        var content=document.getElementById('content');
-        var firstBackgroundContainer = document.getElementById('firstBackgroundContainer');
-        var secondBackgroundContainer = document.getElementById('secondBackgroundContainer');
+        var text1=document.getElementById('text1');
+        var text2=document.getElementById('text2');
+        var container = document.querySelector('.container');
         icon.onclick=function(){
             if(mySong.paused){
                 mySong.play();
                 icon.src="media/pause.png";
-                // 첫 번째 배경 이미지 숨기기
-                firstBackgroundContainer.classList.add('hidden');
-                // 두 번째 배경 이미지 표시
-                secondBackgroundContainer.classList.remove('hidden');
+                icon.style.width = "50px"; // 원하는 너비로 설정
+                icon.style.height = "50px"; // 원하는 높이로 설정
                 //숨기기
-                content.style.display = 'none';
+                text1.style.display = 'none';
+                text2.style.display = 'none';
                 logo.style.display = 'none';
+                container.style.backgroundImage = "url('media/image.png')";
             }
             else{
                 mySong.pause();
                 icon.src="media/play.png";
-                // 첫 번째 배경 이미지 표시
-                firstBackgroundContainer.classList.remove('hidden');
-                // 두 번째 배경 이미지 숨기기
-                secondBackgroundContainer.classList.add('hidden');
+                icon.style.width = ""; // 다시 기본 사이즈로 변경 
+                icon.style.height = ""; // 다시 기본 사이즈로 변경 
                 // 다시 표시
-                content.style.display = 'block';
+                text1.style.display = 'block';
+                text2.style.display = 'block';
                 logo.style.display = 'block';
+                container.style.backgroundImage = "url('media/couple2.jpg')";
             }
         }
